7cdcf1b520d90153714128c48cd1d3fe8af76ad3
Suhas Bachhav <59196135+suhasbachhav@users.noreply.github.com>
2025-03-08 05:27:03 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Player from "./components/Player.jsx";
+
+function App() {
+  return (
+    <main>
+      <header>
+        <img src="game-logo.png" alt="Tic Tac Toe" />
+        <h1>Tic-Tac-toe</h1>
+      </header>
+      <div id="game-container">
+        <ol id="players">
+          <Player name="Player 1" symbol="X" />
+          <Player name="Player 2" symbol="O" />
+        </ol>
+      </div>
+    </main>
+  );
+}
+
+export default App;
